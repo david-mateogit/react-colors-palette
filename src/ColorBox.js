fb5806400afb3b3ee8727fdf51564338b4be3de1
david-mateogit <davidmateo@outlook.com>
2019-12-18 19:05:04 -0400

    ColorBox css refactor flexbox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./ColorBox.css";
 
-const ColorBox = ({ background }) => {
+const ColorBox = ({ background: { name, color } }) => {
   return (
-    <div className="ColorBox" style={{ background: `${background.color}` }}>
-      <span>{background.name}</span>
+    <div className="ColorBox" style={{ background: `${color}` }}>
+      <div className="copy-container">
+        <button className="copy-button" type="button">
+          Copy
+        </button>
+        <div className="box-content">
+          <span className="box-content-name">{name}</span>
+          <span className="see-more">More</span>
+        </div>
+      </div>
     </div>
   );
 };
