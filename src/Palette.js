96cc46a07e4f5f4a1b6024642236983f547f064f
david-mateogit <davidmateo@outlook.com>
2019-12-18 16:33:40 -0400

    first steps

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Palette = () => {
+  return (
+    <div className="Palette">
+      {/* Navbar goes here */}
+      <div className="Pallete-colors">{/* color boxes */}</div>
+      {/* footer */}
+    </div>
+  );
+};
+
+export default Palette;
