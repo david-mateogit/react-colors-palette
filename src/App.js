af3e1d451908c5b8580f22895667a07b63ebc9cc
david-mateogit <davidmateo@outlook.com>
2019-12-19 17:12:45 -0400

    generate colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedColors[1]));
   return (
     <div className="App">
       <Palette seedColors={seedColors[1]} />
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,21 +2,52 @@ import React from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 
-const ColorBox = ({ background: { name, color } }) => {
-  return (
-    <CopyToClipboard text={color}>
-      <div className="ColorBox" style={{ background: `${color}` }}>
-        <div className="copy-container">
-          <button className="copy-button" type="button">
-            Copy
-          </button>
-          <div className="box-content">
-            <span className="box-content-name">{name}</span>
-            <span className="see-more">More</span>
+class ColorBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false,
+    };
+  }
+
+  changeCopyState = () => {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => {
+        this.setState({ copied: false });
+      }, 1000);
+    });
+  };
+
+  render() {
+    const {
+      background: { name, color },
+    } = this.props;
+    const { copied } = this.state;
+
+    return (
+      <CopyToClipboard text={color} onCopy={this.changeCopyState}>
+        <div className="ColorBox" style={{ background: `${color}` }}>
+          <div
+            className={`copy-overlay ${copied && "show"}`}
+            style={{ background: `${color}` }}
+          />
+          <div className={`copy-msg ${copied && "show"}`}>
+            <h1>Copied!</h1>
+            <p>{color}</p>
+          </div>
+
+          <div className="copy-container">
+            <button className="copy-button" type="button">
+              Copy
+            </button>
+            <div className="box-content">
+              <span className="box-content-name">{name}</span>
+              <span className="see-more">More</span>
+            </div>
           </div>
         </div>
-      </div>
-    </CopyToClipboard>
-  );
-};
+      </CopyToClipboard>
+    );
+  }
+}
 export default ColorBox;
