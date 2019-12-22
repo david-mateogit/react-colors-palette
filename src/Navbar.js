9623529622eca468fac624fa5b5d5074d8a22689
david-mateogit <davidmateo@outlook.com>
2019-12-22 11:37:14 -0400

    added select and material ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,28 @@
 import React from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 import "./Navbar.css";
 
-const Navbar = ({ changeLevel, level }) => {
+const Navbar = ({ changeLevel, level, changeFormat }) => {
+  const [format, setFormat] = React.useState("hex");
+  const firstUpdate = React.useRef(true);
+
+  const handleChange = async e => {
+    setFormat(e.target.value);
+  };
+
+  React.useLayoutEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+
+    changeFormat(format);
+  });
+
   return (
     <header className="Navbar">
       <div className="logo">
@@ -21,6 +39,13 @@ const Navbar = ({ changeLevel, level }) => {
             step={100}
           />
         </div>
+      </div>
+      <div className="select-container">
+        <Select value={format} onChange={handleChange}>
+          <MenuItem value="hex">HEX - #FFF</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0) </MenuItem>
+        </Select>
       </div>
     </header>
   );
--- src/Palette.js
+++ src/Palette.js
@@ -6,16 +6,20 @@ import "./Palette.css";
 
 const Palette = ({ palette }) => {
   const [level, setLevel] = React.useState(500);
+  const [format, setFormat] = React.useState("hex");
   const colorBoxes = palette.colors[level].map(c => {
-    return <ColorBox key={c.name} background={{ color: c.hex, name: c.name }} />;
+    return <ColorBox key={c.name} background={{ color: c[format], name: c.name }} />;
   });
 
   const changeLevel = newLevel => {
     return setLevel(newLevel);
   };
+  const changeFormat = val => {
+    setFormat(val);
+  };
   return (
     <div className="Palette">
-      <Navbar changeLevel={changeLevel} level={level} />
+      <Navbar changeLevel={changeLevel} level={level} changeFormat={changeFormat} />
       <div className="Palette-colors">{colorBoxes}</div>
       {/* footer */}
     </div>
